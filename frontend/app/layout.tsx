de9c4d575a2be938797bfd1b17715147f83c6706
david-dozer <dgmendoza1015@gmail.com>
2025-02-20 22:56:00 -0600

    fonts all setup, time to start connecting backend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,12 +7,23 @@ import Header from './components/Header'      // Added import for Header
 export const metadata = {
   title: 'NFL Free Agency AI',
   description: 'A Next.js project replicating SB Admin 2',
+  link: [
+    {
+      rel: 'stylesheet',
+      href: '/vendor/fontawesome-free/css/all.min.css', // Make sure this path is correct
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
+    },
+  ],
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
+      
       {/* <Header /> */}
       <body>
         <div id="wrapper">
